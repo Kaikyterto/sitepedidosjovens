481c2252cd3f4c71d4a5aa3011ae85a312af8a98
Kaikyterto <kaikyterto9@gmail.com>
2026-03-29 12:09:54 -0300

    atualizando cardápio

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -20,7 +20,7 @@ const refrigerante = new Produto("Refrigerante", 1.0);
 const torta = new Produto("Torta de Frango", 4.0);
 const xerem = new Produto("Xerém com galinha", 5.0);
 
-const produtos_hoje = [brownie_texas, cafe];
+const produtos_hoje = [cookies_p, cookies_m, cookies_g, refrigerante];
 
 const div_principal = document.getElementById("principal");
 const btn_nome = document.getElementById("btn_nome");
