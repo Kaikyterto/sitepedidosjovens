dcbb3c0c0c0a5158e2bd4c804a1f112b4a9cd77b
Kaikyterto <kaikyterto9@gmail.com>
2026-04-12 14:40:45 -0300

    att cardapio

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -19,8 +19,9 @@ const cuzcuz = new Produto("Cuzcuz", 5.0);
 const refrigerante = new Produto("Refrigerante", 1.0);
 const torta = new Produto("Torta de Frango", 4.0);
 const xerem = new Produto("Xerém com galinha", 5.0);
+const tapioca = new Produto("Tapioca", 3.0);
 
-const produtos_hoje = [brownie, cafe];
+const produtos_hoje = [tapioca, cafe];
 
 const div_principal = document.getElementById("principal");
 const btn_nome = document.getElementById("btn_nome");
