53ae77f11b3a9415fdf95c8ec2a36d5bfaacbcea
Kaikyterto <kaikyterto9@gmail.com>
2026-05-02 17:38:18 -0300

    conectando ao banco

--- app/models/db.py
+++ app/models/db.py
@@ -3,8 +3,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
+
 def get_connection():
-    return psycopg2.connect(os.environ["DATABASE_URL"])
+    psycopg2.connect(
+    os.environ["DATABASE_URL"],
+    sslmode="require"
+)
 
 conn = get_connection()
 cursor = conn.cursor()
